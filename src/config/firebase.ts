417aca12ff501b76bce58415c28b0b67f1255685
speedysweeties-ca <speedysweeties@gmail.com>
2026-09-08 16:01:00 -0400

    Use non-network Firebase stub during tests

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,22 @@
 import { cert, getApps, initializeApp, type ServiceAccount } from "firebase-admin/app";
-import { getMessaging } from "firebase-admin/messaging";
+import { getMessaging, type Messaging } from "firebase-admin/messaging";
 import { env } from "./env";
 
-const serviceAccount = JSON.parse(env.FIREBASE_SERVICE_ACCOUNT_JSON);
-const firebaseApp = getApps().length > 0
-  ? getApps()[0]
-  : initializeApp({
-      credential: cert(serviceAccount as ServiceAccount)
-    });
+const createTestMessaging = (): Messaging =>
+  ({
+    send: async () => "test-message-id"
+  }) as unknown as Messaging;
 
-export const messaging = getMessaging(firebaseApp);
+const createFirebaseMessaging = (): Messaging => {
+  const serviceAccount = JSON.parse(env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  const firebaseApp = getApps().length > 0
+    ? getApps()[0]
+    : initializeApp({
+        credential: cert(serviceAccount as ServiceAccount)
+      });
+
+  return getMessaging(firebaseApp);
+};
+
+export const messaging =
+  env.NODE_ENV === "test" ? createTestMessaging() : createFirebaseMessaging();
