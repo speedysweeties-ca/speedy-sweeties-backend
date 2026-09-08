5c21a9a9b1ab77ad7bbcf5571061b8d55670e77e
speedysweeties-ca <speedysweeties@gmail.com>
2026-09-08 18:58:59 -0400

    Restore driver pins with initials

--- speedy-dispatcher/src/main.tsx
+++ speedy-dispatcher/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { RoutingPreviewEnhancer } from './RoutingPreviewEnhancer.tsx'
+import { installDriverMarkerPinFix } from './driverMarkerPinFix.ts'
+
+installDriverMarkerPinFix()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
